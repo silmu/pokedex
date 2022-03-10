8a48bbaf4934b886c85d94c0031d24d4a0c9fcad
silmu <svetlana.raitina@gmail.com>
2022-03-10 21:41:59 +0200

    Refactored and added html for filter by type

--- Pokedex/main.js
+++ Pokedex/main.js
@@ -4,71 +4,79 @@ class FetchWrapper {
   }
 
   get(endpoint) {
-    return fetch(this.baseURL + endpoint).then((response) => response.json());
+    return fetch(this.baseURL + endpoint)
+      .then((response) => response.json())
+      .catch((err) => alert('Not found'));
   }
 }
 
 const API = new FetchWrapper('https://pokeapi.co/api/v2/pokemon/');
 //'https://pokeapi.co/api/v2/pokemon?limit=100&&offset=200
 
-let pokemons = [
-  'pikachu',
-  'ditto',
-  'charmander',
-  'eevee',
-  'mewtwo',
-  'squirtle',
-  'bulbasaur',
-  'snorlax',
-  'dragonite',
-];
-let imgLink;
+let pokemons, imgLink, divCard, divImg, divTitle, cardsContainer;
+
+const createCard = () => {
+  //Create card element, card image and card title
+  divCard = document.createElement('div');
+  //Assign classes
+  divCard.className = 'card';
+  divImg = document.createElement('div');
+  divImg.className = 'card-img';
+  divTitle = document.createElement('div');
+  divTitle.className = 'card-title';
+  //Get container for cards
+  cardsContainer = document.querySelector('.cards');
+};
 
 const fetchPokemons = (pokemons) => {
   pokemons.forEach((pokemon) => {
-    API.get(pokemon).then((data) => {
-      //Create card element, card image and card title
-      const divCard = document.createElement('div');
-      //Assign classes
-      divCard.className = 'card';
-      const divImg = document.createElement('div');
-      divImg.className = 'card-img';
-      const divTitle = document.createElement('div');
-      divTitle.className = 'card-title';
-      //Get container for cards
-      const cardsContainer = document.querySelector('.cards');
-      //Get link to a pokemon image
-      imgLink = data.sprites.other['official-artwork'].front_default;
-      // imgLink = data.sprites.other['dream_world']['front_default'];
+    API.get(pokemon)
+      .then((data) => {
+        createCard();
+        //Get link to a pokemon image
+        imgLink = data.sprites.other['official-artwork'].front_default;
+        // imgLink = data.sprites.other['dream_world']['front_default'];
 
-      //Add new card elements and assign it appropriate link and name
-      divCard.appendChild(divImg);
-      divImg.style.backgroundImage = `url('${imgLink}')`;
-      divCard.appendChild(divTitle);
-      let pokemonType = data.types[0].type.name;
-      divTitle.textContent = `${pokemon} (${pokemonType})`;
-      cardsContainer.appendChild(divCard);
-    });
+        //Add new card elements and assign it appropriate link and name
+        divCard.appendChild(divImg);
+        divImg.style.backgroundImage = `url('${imgLink}')`;
+        divCard.appendChild(divTitle);
+        let pokemonType = data.types[0].type.name;
+        divTitle.textContent = `${pokemon} (${pokemonType})`;
+        cardsContainer.appendChild(divCard);
+      })
+      .catch((err) => console.log(err));
   });
 };
 
 // Alternative solution
-const API2 = new FetchWrapper('https://pokeapi.co/api/v2/pokemon');
-API.get('?limit=20&&offset=200').then((data) => {
-  const fetches = data.results.map((element) => {
-    return fetch(element.url).then((res) => res.json());
-  });
-  Promise.all(fetches).then((results) =>
-    console.log('Promise all results', results)
-  );
-});
+// const API2 = new FetchWrapper('https://pokeapi.co/api/v2/pokemon');
+// API2.get('?limit=20&&offset=200').then((data) => {
+//   const fetches = data.results.map((element) => {
+//     return fetch(element.url).then((res) => res.json());
+//   });
+//   Promise.all(fetches).then((results) =>
+//     console.log('Promise all results', results)
+//   );
+// });
 
 //Search
 const search = document.querySelector('#search');
 
 const findPokemon = () => {
   console.log('Button search is clicked ' + search.value);
-  pokemons = [search.value];
+  pokemons = [search.value.toLowerCase()];
+
   fetchPokemons(pokemons);
+
+  search.value = '';
 };
 search.addEventListener('change', findPokemon);
+
+// Filter
+const filter = document.querySelector('#filter');
+
+filter.addEventListener('change', () => {
+  const type = filter.value;
+  console.log(type);
+});
